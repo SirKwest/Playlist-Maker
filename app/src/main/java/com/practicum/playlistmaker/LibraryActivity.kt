80eb8a5240eb7bf8253ebea78655834afc2d8ad6
Yaroslav Fedorov <dev10@max.id>
2024-01-29 16:03:16 +0400

    Sprint 8: Initial task completed.

---
+++ app/src/main/java/com/practicum/playlistmaker/LibraryActivity.kt
@@ -0,0 +1,10 @@
+package com.practicum.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settingsButton = findViewById<Button>(R.id.settings);
+        settingsButton.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    SettingsActivity::class.java
+                )
+            )
+        }
+
+        val libraryButtonListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startActivity(Intent(this@MainActivity, LibraryActivity::class.java))
+            }
+        }
+        val libraryButton = findViewById<Button>(R.id.library);
+        libraryButton.setOnClickListener(libraryButtonListener);
+
+        val searchButton = findViewById<Button>(R.id.search);
+        searchButton.setOnClickListener { startActivity(Intent(this@MainActivity, SearchActivity::class.java)) }
     }
 }
