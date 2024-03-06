3b34a03ebe401bd9c39750d33397badab5de8da8
Iaroslav Fedorov <fed.kwest@gmail.com>
2024-03-06 19:44:50 +0400

    Sprint 8: Text at buttons corrected and activities were added.

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,9 +2,11 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
     }
 }
