e9a18d2adbfff92b50d5ec52b444433c7147cf11
Iaroslav Fedorov <fed.kwest@gmail.com>
2024-06-03 16:01:48 +0400

    Added functionality for buttons at settings activity.

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,5 +14,36 @@ class SettingsActivity : AppCompatActivity() {
 
         val backButton = findViewById<ImageView>(R.id.arrow_back);
         backButton.setOnClickListener { super.finish() }
+
+        val shareButton = findViewById<FrameLayout>(R.id.share_button);
+        shareButton.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.practicum_url))
+                type = "text/plain"
+            }
+            startActivity(Intent.createChooser(sendIntent, null))
+        }
+
+        val supportButton = findViewById<FrameLayout>(R.id.support_button);
+        supportButton.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.developer_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_title_placeholder))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text_placeholder))
+            }
+            startActivity(Intent.createChooser(sendIntent, null))
+        }
+
+        val userAgreementButton = findViewById<FrameLayout>(R.id.user_agreement_button);
+        userAgreementButton.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(getString(R.string.user_agreement_link))
+            }
+            startActivity(Intent.createChooser(sendIntent, null))
+        }
     }
 }
