dfeb2d525f4d4c05d5ff5c6af2bec8226c16bc0a
Iaroslav Fedorov <fed.kwest@gmail.com>
2024-09-19 20:43:55 +0400

    Sprint 18. Library activity refactored to tab layout and added some placeholder info.

---
+++ app/src/main/java/com/practicum/playlistmaker/library/ui/PlaylistsFragment.kt
@@ -0,0 +1,31 @@
+package com.practicum.playlistmaker.library.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.databinding.LibraryPlaylistsFragmentBinding
+
+class PlaylistsFragment : Fragment() {
+    private var _binding: LibraryPlaylistsFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = LibraryPlaylistsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance() = PlaylistsFragment()
+    }
+}
